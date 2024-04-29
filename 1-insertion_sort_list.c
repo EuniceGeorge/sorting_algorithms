9f34a8644579ba228b8ac15b35eb3a36c5fc7647
BugFix.exe <bugfix.exe@gmail.com>
2024-04-29 18:10:53 +0100

    solution to the selcection sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,20 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - a function that implements insertion sort
+ * @list: a double linked list of type listint_t
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *swap;
+	temp = *list;
+	int swap;
+
+	while (temp)
+	{
+
+		temp = temp->next;
+	}
+
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ * smallest - a function that returns pos of smallest value
+ * @arr: the address to an array
+ * @k: the current index
+ * @n: size of the array
+ * Return: the pos of the small value
+ */
+
+int smallest(int arr[], int k, int n)
+{
+	int pos, small = arr[k], i;
+
+	for (i = k + 1; i < n; i++)
+	{
+		if (arr[i] < small)
+		{
+			small = arr[i];
+			pos = i;
+		}
+	}
+	return (pos);
+}
+
+/**
+ * selection_sort - a function that implements selction sort
+ * @array: an unsorted array
+ * @size: the size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t  k, pos, temp;
+
+	for (k = 0; k < size; k++)
+	{
+		pos = smallest(array, k, size);
+		temp = array[k];
+		array[k] = array[pos];
+		array[pos] = temp;
+		print_array(array, size);
+	}
+}
