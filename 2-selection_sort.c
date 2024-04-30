a766beaed0c50778a62b95a0a1067e62085524ac
BugFix.exe <bugfix.exe@gmail.com>
2024-04-30 09:58:33 +0100

    debugging

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,17 +14,14 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
-			{
 				min = j;
-				tmp = array[min];
-				array[min] = array[i];
-				array[i] = tmp;
-				print_array(array, size);
-			}
 		}
+		tmp = array[min];
+		array[min] = array[i];
+		array[i] = tmp;
+		print_array(array, size);
 	}
 }
