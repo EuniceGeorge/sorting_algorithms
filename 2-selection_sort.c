2bd38843128ef98bbf6c8730f7e39680441991dd
Eunice George <georgeunice408@gmail.com>
2024-04-30 00:45:15 -0700

    task 0

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,43 +1,30 @@
 #include "sort.h"
 
 /**
- * smallest - a function that returns pos of smallest value
- * @arr: the address to an array
- * @k: the current index
- * @n: size of the array
- * Return: the pos of the small value
- */
-
-int smallest(int arr[], int k, int n)
-{
-	int pos, small = arr[k], i;
-
-	for (i = k + 1; i < n; i++)
-	{
-		if (arr[i] < small)
-		{
-			small = arr[i];
-			pos = i;
-		}
-	}
-	return (pos);
-}
-
-/**
  * selection_sort - a function that implements selction sort
  * @array: an unsorted array
  * @size: the size of the array
  */
+
 void selection_sort(int *array, size_t size)
 {
-	size_t  k, pos, temp;
+	size_t i, j, tmp;
+	int min;
 
-	for (k = 0; k < size - 1; k++)
+	for (i = 0; i < size - 1; i++)
 	{
-		pos = smallest(array, k, size);
-		temp = array[k];
-		array[k] = array[pos];
-		array[pos] = temp;
-		print_array(array, size);
+		min = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+				tmp = array[min];
+				array[min] = array[i];
+				array[i] = tmp;
+			}
+		}
+			print_array(array, size);
 	}
 }
