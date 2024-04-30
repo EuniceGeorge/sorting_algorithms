8933da481b895f49383151c51c41025b564fc5b8
BugFix.exe <bugfix.exe@gmail.com>
2024-04-30 13:11:58 +0100

    debugging

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,14 +7,25 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *swap;
+	listint_t *temp, *prev_node, *next_node; /**prev_addr*/
+
 	temp = *list;
-	int swap;
 
 	while (temp)
 	{
+		prev_node = temp->prev;
+		next_node = temp->next;
 
+		while ((next_node->n < prev_node->n) && (prev_node != NULL))
+			prev_node = prev_node->prev;
+		/**
+		prev_node->next = next_node->next;
+		next_node->next = prev_node;
+		prev_addr = prev_node->prev;
+		prev_node->prev = next_node;
+		next_node->next = prev_node;
+		next_node->prev = prev_addr;
+		*/
 		temp = temp->next;
 	}
-
 }
